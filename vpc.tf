184e938da8bae96c4587d5042784ffaa8c9392c9
Ulan Yisaev <ulan.yisaev@lhv.ee>
2025-05-14 14:10:15 +0300

    fixes

--- vpc.tf
+++ vpc.tf
@@ -38,8 +38,49 @@ locals {
   }
 }
 
-# Remove all VPC Endpoint resources - they are assumed to exist in the target VPC
+resource "aws_security_group" "vpc_endpoints_sg" { # Renamed to avoid conflict if original was "vpc_endpoints"
+  name        = "${var.name}-vpc-endpoints-sg" // var.name is the Langfuse deployment name
+  description = "Security group for VPC interface endpoints used by Langfuse EKS"
+  vpc_id      = local.vpc_config.vpc_id // Uses the existing VPC ID
+
+  ingress {
+    description = "Allow HTTPS from VPC CIDR"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [local.vpc_config.vpc_cidr_block] // Allow from within the VPC
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${local.tag_name} VPC Endpoints SG" // local.tag_name from module's locals.tf
+  }
+}
+
+resource "aws_vpc_endpoint" "sts" {
+  vpc_id              = local.vpc_config.vpc_id
+  service_name        = "com.amazonaws.${data.aws_region.current.name}.sts"
+  vpc_endpoint_type   = "Interface"
+  private_dns_enabled = true
+  subnet_ids          = local.vpc_config.private_subnets // Place endpoint ENIs in your private subnets
+
+  security_group_ids = [
+    aws_security_group.vpc_endpoints_sg.id,
+    // You might also need to allow traffic from the EKS cluster security group
+    // if specific pod-to-endpoint SG rules are more restrictive.
+    // For STS, typically allowing from the VPC CIDR to the endpoint SG on 443 is enough.
+  ]
+
+  tags = {
+    Name = "${local.tag_name} STS VPC Endpoint"
+  }
+}
+# Remove all other VPC Endpoint resources - they are assumed to exist in the target VPC
 # Removed:
-# - resource "aws_vpc_endpoint" "sts" { ... }
 # - resource "aws_vpc_endpoint" "s3" { ... }
-# - resource "aws_security_group" "vpc_endpoints" { ... }
